70ebcfedf2fab6d5a747d30e483de49f841ba84a
Tek4459 <tek4553lol@gmail.com>
2024-10-05 19:06:32 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+
+symbols = fjdsfklgdfkghuif3294284234ijsudhyw87erey32w89473028409324rSDASFDASDFASDKFAPSKDLFOKJSDOIIEROWEOAWSDLKOPASDOASDOASD 
+posword = ""
+for i in range(12):
+    pasword += random.choice(symbols)
+print(pasword)    
